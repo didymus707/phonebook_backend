31d35ea1d0404dfca351e75473ec56ad570b10dc
Adewale Orotayo <didymus7007@gmail.com>
2020-11-25 17:34:06 +0100

    Added a random id generator Implemented error handling for new entries

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const { request, response } = require('express');
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 let persons = [
   {
     id: 1,
@@ -22,8 +24,18 @@ let persons = [
     id: 4,
     name: "Emmanuel",
     number: "07062357670"
+  },
+  {
+    id: 5,
+    name: "Fadekemi",
+    number: "08034274531"
   }
 ]
+
+const generateId = () => {
+  const ids = persons.map(person => person.id);
+  return Math.floor(Math.random() * 100);
+};
 
 app.get('/', (request, response) => {
   response.send("<h1>Welcome to Didymus' Phonebook Backend App</h1>")
@@ -52,6 +64,30 @@ app.delete('/api/persons/:id', (request, response) => {
   persons = persons.filter(person => person.id !== id);
 
   response.status(204).end();
+});
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body;
+  const name = persons.find(person => person.name === body.name);
+  const number = persons.find(person => person.number === body.number);
+
+  if (!body.name || !body.number) {
+    return response.status(404).json({
+      error: 'Name or number is missing'
+    })
+  } else if (name || number) {
+    return response.status(404).json({
+      error: 'Name or Number already exists in the phonebook'
+    })
+  }
+  
+  const person = {
+    id: generateId(),
+    name: body.name,
+    number: body.number
+  }
+  persons.push(person);
+  response.json(person);
 });
 
 const PORT = 3001;
